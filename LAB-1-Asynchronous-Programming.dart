d3ef831af06900bae12812de2e4dec0549d4134d
Beth-Gg <149475874+Beth-Gg@users.noreply.github.com>
2024-03-05 23:34:46 +0300

    Update LAB-1-Asynchronous-Programming.dart

--- LAB-1-Asynchronous-Programming.dart
+++ LAB-1-Asynchronous-Programming.dart
@@ -1,1 +1,15 @@
+Future <int> add(int a, int b) async {
+  await Future.delayed(Duration(seconds: 3));
+  return(a + b);
+}
+void main() async{
+  await (add(3,4)).then((value) => print("Their sum is $value"));
 
+  future <void> add(int a, int b) async {
+  await future.delayed(Duration(seconds: 3));
+  print(a + b);
+  }
+  void funct() async{
+    await (add(3,4));
+  }
+}

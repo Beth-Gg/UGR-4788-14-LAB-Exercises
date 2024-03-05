9925801af8b60bd44972cca80e6d0a4993b8239d
Beth-Gg <149475874+Beth-Gg@users.noreply.github.com>
2024-03-05 23:14:34 +0300

    Update LAB-1-Collections.dart

--- LAB-1-Collections.dart
+++ LAB-1-Collections.dart
@@ -8,4 +8,12 @@ void main(){
     }
     return max;
   }
+
+    void mapList(Map<String, String> map){
+    for (var i in map.keys){
+      print("Key: $i");
+      var val = map[i];
+      print("Value: $val");
+    }
+  }
 }

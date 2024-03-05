e021269889e0bc3468c47927c1ef9ac9e4bf3f9a
Beth-Gg <149475874+Beth-Gg@users.noreply.github.com>
2024-03-05 23:15:20 +0300

    Update LAB-1-Collections.dart

--- LAB-1-Collections.dart
+++ LAB-1-Collections.dart
@@ -16,4 +16,17 @@ void main(){
       print("Value: $val");
     }
   }
+
+    List lst1 = [];
+  List removeDuplicates(List lst){
+    for (int i=0; i<lst.length; i++){
+      if ((lst1.contains(lst[i]))) {
+        continue;
+      }
+      else{
+        lst1.add(lst[i]);
+      }
+      }
+    return lst1 ;
+    }
 }

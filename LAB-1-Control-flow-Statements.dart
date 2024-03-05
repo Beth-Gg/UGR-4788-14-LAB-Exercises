9ed0244f49ca516394ff42d04d88843780d439da
Beth-Gg <149475874+Beth-Gg@users.noreply.github.com>
2024-03-05 17:31:53 +0300

    Update LAB-1-Control-flow-Statements.dart

--- LAB-1-Control-flow-Statements.dart
+++ LAB-1-Control-flow-Statements.dart
@@ -1,1 +1,28 @@
+void main() {
 
+  var day = 1;
+  switch (day) {
+    case '1':
+      print("monday");
+      break;
+    case '2':
+      print("tuesday");
+      break;
+    case '3':
+      print("wednesday");
+      break;
+    case '4':
+      print("thursday");
+      break;
+    case '5':
+      print("friday");
+      break;
+    case '6':
+      print("saturday");
+      break;
+    case '7':
+      print("sunday");
+      break;
+
+    default:
+  }

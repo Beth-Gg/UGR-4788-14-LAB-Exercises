0c43aa6b14677812acabe120af32e69139691ae2
Beth-Gg <149475874+Beth-Gg@users.noreply.github.com>
2024-03-05 21:33:30 +0300

    Update LAB-1-Functions.dart

--- LAB-1-Functions.dart
+++ LAB-1-Functions.dart
@@ -1,1 +1,10 @@
-
+void main() {
+  int average(List lisst) {
+    num sum = 0;
+    for (int i = 0; i < lisst.length; i++) {
+      sum = sum + lisst[i];
+    }
+    num avrg = sum / (lisst.length);
+    return avrg;
+  }
+}

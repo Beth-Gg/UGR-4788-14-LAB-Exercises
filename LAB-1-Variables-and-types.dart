8db4c767f9dfbdb04807d4ad0ac342a450fb4cb4
Beth-Gg <149475874+Beth-Gg@users.noreply.github.com>
2024-03-05 16:41:14 +0300

    Create LAB-1-Variables-and-types.dart

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,47 @@
+// import 'dart:async';
+// import 'dart:svg';
+import "dart:io";
+
+int a = 2;
+int b = 3;
+int c = 4;
+
+void main() {
+  print(a + b + c);
+
+  String strr = "abcdefgh";
+  int length = strr.length;
+  print(length);
+
+  var day = ;
+  switch (day) {
+    case '1':
+      print("monday");
+      break;
+    case '2':
+      print("monday");
+      break;
+    case '3':
+      print("monday");
+      break;
+    case '4':
+      print("monday");
+      break;
+    case '5':
+      print("monday");
+      break;
+    case '6':
+      print("monday");
+      break;
+    case '7':
+      print("Sunday");
+      break;
+
+    default:
+  }
+
+  for (var i = 1; i < 10; i++) {
+    print (i + (i - 1));
+
+  }
+}

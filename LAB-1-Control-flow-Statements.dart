4520931ed60a3fee11efebde0a15cbdf97e0ee69
Beth-Gg <149475874+Beth-Gg@users.noreply.github.com>
2024-03-05 17:33:59 +0300

    Update LAB-1-Control-flow-Statements.dart

--- LAB-1-Control-flow-Statements.dart
+++ LAB-1-Control-flow-Statements.dart
@@ -25,4 +25,16 @@ void main() {
       break;
 
     default:
+
+  var list = [];
+
+  for (var i = 0; i < 10; i++) {
+    if(i < 2){
+      list.add(i);
+      print(list[i]);
+      continue;
+    }
+    list.add(list[i-1] + list[i-2]);
+    print(list[i]);
   }
+}

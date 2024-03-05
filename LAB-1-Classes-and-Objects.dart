9b9a81593c099e25dad95fc1d04d28e35b6c47d5
Beth-Gg <149475874+Beth-Gg@users.noreply.github.com>
2024-03-05 23:31:26 +0300

    Update LAB-1-Classes-and-Objects.dart

--- LAB-1-Classes-and-Objects.dart
+++ LAB-1-Classes-and-Objects.dart
@@ -9,4 +9,14 @@ void main() {
   print('Brand: ${Car1.brand}');
   print('Model: ${Car1.model}');
   print('Year: ${Car1.year}');
+  Car1.vroom();
+
+   void vroom(){
+    print('Vroom Vroom');
+  }
+}
+
+class ElectricCar extends Car {
+  int batteryLife;
+  ElectricCar(String brand, String model, int year, this.batteryLife): super(brand, model, year);
 }
